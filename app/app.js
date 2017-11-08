037b453cb62b3fcd415b8d1893042a76f3c904f5
Samir <samirshah@deloitte.com>
2017-11-08 21:33:22 +0530

    changes

--- app/app.js
+++ app/app.js
@@ -1,124 +1,126 @@
 (function () {
-  'use strict';
-  var app = angular.module('app', [], function config($httpProvider) {
-    $httpProvider.interceptors.push('AuthInterceptor');
-  });
-
-  app.constant('API_URL', 'http://localhost:3000');
-
-  app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory) {
     'use strict';
-    var vm = this;
-    vm.getRandomUser = getRandomUser;
-    vm.login = login;
-    vm.logout = logout;
-
-    // initialization
-    UserFactory.getUser().then(function success(response) {
-      vm.user = response.data;
+    var app = angular.module('app', [], function config($httpProvider) {
+        $httpProvider.interceptors.push('AuthInterceptor');
     });
 
-    function getRandomUser() {
-      RandomUserFactory.getUser().then(function success(response) {
-        vm.randomUser = response.data;
-      }, handleError);
-    }
+    app.constant('API_URL', 'https://mysterious-island-10457.herokuapp.com:3000');
 
-    function login(username, password) {
-      UserFactory.login(username, password).then(function success(response) {
-        vm.user = response.data.user;
-      }, handleError);
-    }
+    app.controller('MainCtrl', function MainCtrl(RandomUserFactory, UserFactory) {
+        'use strict';
+        var vm = this;
+        vm.getRandomUser = getRandomUser;
+        vm.login = login;
+        vm.logout = logout;
 
-    function logout() {
-      UserFactory.logout();
-      vm.user = null;
-    }
+        // initialization
+        UserFactory.getUser().then(function success(response) {
+            vm.user = response.data;
+        });
 
-    function handleError(response) {
-      alert('Error: ' + response.data);
-    }
-  });
+        function getRandomUser() {
+            RandomUserFactory.getUser().then(function success(response) {
+                vm.randomUser = response.data;
+            }, handleError);
+        }
 
-  app.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
-    'use strict';
-    return {
-      getUser: getUser
-    };
+        function login(username, password) {
+            UserFactory.login(username, password).then(function success(response) {
+                vm.user = response.data.user;
+            }, handleError);
+        }
 
-    function getUser() {
-      return $http.get(API_URL + '/random-user');
-    }
-  });
+        function logout() {
+            UserFactory.logout();
+            vm.user = null;
+        }
 
-  app.factory('UserFactory', function UserFactory($http, API_URL, AuthTokenFactory, $q) {
-    'use strict';
-    return {
-      login: login,
-      logout: logout,
-      getUser: getUser
-    };
+        function handleError(response) {
+            alert('Error: ' + response.data);
+        }
+    });
 
-    function login(username, password) {
-      return $http.post(API_URL + '/login', {
-        username: username,
-        password: password
-      }).then(function success(response) {
-        AuthTokenFactory.setToken(response.data.token);
-        return response;
-      });
-    }
+    app.factory('RandomUserFactory', function RandomUserFactory($http, API_URL) {
+        'use strict';
+        return {
+            getUser: getUser
+        };
 
-    function logout() {
-      AuthTokenFactory.setToken();
-    }
+        function getUser() {
+            return $http.get(API_URL + '/random-user');
+        }
+    });
 
-    function getUser() {
-      if (AuthTokenFactory.getToken()) {
-        return $http.get(API_URL + '/me');
-      } else {
-        return $q.reject({ data: 'client has no auth token' });
-      }
-    }
-  });
+    app.factory('UserFactory', function UserFactory($http, API_URL, AuthTokenFactory, $q) {
+        'use strict';
+        return {
+            login: login,
+            logout: logout,
+            getUser: getUser
+        };
 
-  app.factory('AuthTokenFactory', function AuthTokenFactory($window) {
-    'use strict';
-    var store = $window.localStorage;
-    var key = 'auth-token';
+        function login(username, password) {
+            return $http.post(API_URL + '/login', {
+                username: username,
+                password: password
+            }).then(function success(response) {
+                AuthTokenFactory.setToken(response.data.token);
+                return response;
+            });
+        }
 
-    return {
-      getToken: getToken,
-      setToken: setToken
-    };
+        function logout() {
+            AuthTokenFactory.setToken();
+        }
 
-    function getToken() {
-      return store.getItem(key);
-    }
+        function getUser() {
+            if (AuthTokenFactory.getToken()) {
+                return $http.get(API_URL + '/me');
+            } else {
+                return $q.reject({
+                    data: 'client has no auth token'
+                });
+            }
+        }
+    });
 
-    function setToken(token) {
-      if (token) {
-        store.setItem(key, token);
-      } else {
-        store.removeItem(key);
-      }
-    }
+    app.factory('AuthTokenFactory', function AuthTokenFactory($window) {
+        'use strict';
+        var store = $window.localStorage;
+        var key = 'auth-token';
 
-  });
+        return {
+            getToken: getToken,
+            setToken: setToken
+        };
 
-  app.factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
-    'use strict';
-    return {
-      request: addToken
-    };
+        function getToken() {
+            return store.getItem(key);
+        }
 
-    function addToken(config) {
-      var token = AuthTokenFactory.getToken();
-      if (token) {
-        config.headers = config.headers || {};
-        config.headers.Authorization = 'Bearer ' + token;
-      }
-      return config;
-    }
-  });
+        function setToken(token) {
+            if (token) {
+                store.setItem(key, token);
+            } else {
+                store.removeItem(key);
+            }
+        }
+
+    });
+
+    app.factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
+        'use strict';
+        return {
+            request: addToken
+        };
+
+        function addToken(config) {
+            var token = AuthTokenFactory.getToken();
+            if (token) {
+                config.headers = config.headers || {};
+                config.headers.Authorization = 'Bearer ' + token;
+            }
+            return config;
+        }
+    });
 })();
--- server.js
+++ server.js
@@ -7,42 +7,46 @@ var expressJwt = require('express-jwt');
 var jwtSecret = 'fjkdlsajfoew239053/3uk';
 
 var user = {
-  username: 'samirshah@deloitte.com',
-  password: 'Test@123'
+    username: 'samirshah@deloitte.com',
+    password: 'Test@123'
 };
 
 var app = express();
 
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 app.use(bodyParser.json());
-app.use(expressJwt({ secret: jwtSecret }).unless({ path: [ '/login' ]}));
+app.use(expressJwt({
+    secret: jwtSecret
+}).unless({
+    path: ['/login']
+}));
 
 
 app.post('/login', authenticate, function (req, res) {
-  var token = jwt.sign({
-    username: user.username
-  }, jwtSecret);
-  res.send({
-    token: token,
-    user: user
-  });
+    var token = jwt.sign({
+        username: user.username
+    }, jwtSecret);
+    res.send({
+        token: token,
+        user: user
+    });
 });
 
 
-app.listen(3000, function () {
-  console.log('App listening on localhost:3000');
+app.listen(8080, function () {
+    console.log('App listening on localhost:8080');
 });
 
 // UTIL FUNCTIONS
 
 function authenticate(req, res, next) {
-  var body = req.body;
-  if (!body.username || !body.password) {
-    res.status(400).end('Must provide username or password');
-  } else if (body.username !== user.username || body.password !== user.password) {
-    res.status(401).end('Username or password incorrect');
-  } else {
-    next();
-  }
+    var body = req.body;
+    if (!body.username || !body.password) {
+        res.status(400).end('Must provide username or password');
+    } else if (body.username !== user.username || body.password !== user.password) {
+        res.status(401).end('Username or password incorrect');
+    } else {
+        next();
+    }
 }
