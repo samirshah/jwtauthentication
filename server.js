892b2265c96f5f838e7e5a48061d38c1321440ff
Samir <samirshah@deloitte.com>
2017-11-08 21:59:50 +0530

    changes

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.post('/login', authenticate, function (req, res) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('App listening on localhost:3000');
 });
 
